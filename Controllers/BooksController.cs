f33a1cd4372f3b77802614858f231aa3ac389da8
Gabor Szabo <gabor.szabo86@hotmail.com>
2020-03-03 02:58:09 +0100

    Connecting MongoDb to the bookstore

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using BooksApi.Model;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// "ConnectionString": "mongodb://localhost:27017",
+namespace BooksApi.Services
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> GetAction() =>
+            _bookService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            _bookService.Create(book);
+
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Update(id, bookIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Remove(book.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## First ASP.NET Core 3.1 Web API with MongoDb
+
+This repo was created by [this tutorial](https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mongo-app?view=aspnetcore-3.1&tabs=visual-studio-code).
